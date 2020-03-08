7084f5f298738736dab15f5ee00ea4e721331e29
Stuti <stutibansal99@gmail.com>
2020-03-08 18:42:15 +0530

    third commit

--- src/com/email/Email.java
+++ src/com/email/Email.java
@@ -1,5 +1,29 @@
 package com.email;
 
-public class Email {
+import java.util.Scanner;
 
+public class Email {
+	private String firstName,lastName,password,department,alternateEmail;
+	private int mailBoxCapacity;
+	
+	public Email(String firstName,String lastName)
+	{
+		this.firstName=firstName;
+		this.lastName=lastName;
+		System.out.println("Email Created : "+this.firstName+" "+this.lastName);
+	}
+	private String setDepartment()
+	{
+		System.out.println("Enter the department\n1 for Sales\n2 for Develeoment\n3 Acoounting\n0 for none");
+		Scanner scanner=new Scanner(System.in);
+		int value=scanner.nextInt();
+		if(value==1)
+			return "Sales";
+		else if(value==2)
+			return "Development";
+		else if(value==3)
+			return "Accounting";
+		else
+			return null;
+	}
 }
