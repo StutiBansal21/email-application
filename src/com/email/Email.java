cfbe8c55d33ab4fcf0b997e61d3b6318f74f1b02
Stuti <stutibansal99@gmail.com>
2020-03-08 21:36:16 +0530

    fourth commit

--- src/com/email/Email.java
+++ src/com/email/Email.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Email {
 	private String firstName,lastName,password,department,alternateEmail;
+	private int defaultPasswordLength=10;
 	private int mailBoxCapacity;
 	
 	public Email(String firstName,String lastName)
@@ -11,6 +12,12 @@ public class Email {
 		this.firstName=firstName;
 		this.lastName=lastName;
 		System.out.println("Email Created : "+this.firstName+" "+this.lastName);
+		
+		this.department=setDepartment();
+		System.out.println("Department is "+this.department);
+		
+		this.password=randomPassword(defaultPasswordLength);
+		System.out.println("Your password is : "+this.password);
 	}
 	private String setDepartment()
 	{
@@ -25,5 +32,17 @@ public class Email {
 			return "Accounting";
 		else
 			return null;
+	}
+	
+	private String randomPassword(int length)
+	{
+		String passwordSet="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!£$%^&*";
+		char password[]=new char[length];
+		for(int i=0;i<length;i++)
+		{
+			int ranndom=(int)(Math.random()*passwordSet.length());
+			password[i]=passwordSet.charAt(ranndom);
+		}
+		return new String(password);
 	}
 }
